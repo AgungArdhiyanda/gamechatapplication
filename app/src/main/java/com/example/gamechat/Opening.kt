ea0be5561fb4d0e4cf2aeac9306c2b32fa6c7f1f
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2022-07-10 20:41:08 +0800

    Add project

---
+++ app/src/main/java/com/example/gamechat/Opening.kt
@@ -0,0 +1,11 @@
+package com.example.gamechat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Opening : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opening)
+    }
+}
