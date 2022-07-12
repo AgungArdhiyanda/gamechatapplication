2ff8358e2b7fdec3bea921b6585957f788bbcc43
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2022-07-12 11:46:33 +0800

    Add button to change activity

--- app/src/main/java/com/example/gamechat/MainActivity.kt
+++ app/src/main/java/com/example/gamechat/MainActivity.kt
@@ -3,10 +3,21 @@ package com.example.gamechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<ImageButton>(R.id.addBtn)
+
+        button.setOnClickListener{
+            val intent = Intent(this,Mainpage::class.java)
+            startActivity(intent)
+            finish()
         }
     }
+}
