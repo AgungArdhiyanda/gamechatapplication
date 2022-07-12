7a307054b047797524d43504b96917826ba9e1a3
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2022-07-12 13:44:35 +0800

    Add circle image & remove title bar

--- app/src/main/java/com/example/gamechat/MainActivity.kt
+++ app/src/main/java/com/example/gamechat/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.content.Intent
+import android.view.Window
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<ImageButton>(R.id.addBtn)
@@ -19,5 +21,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+
+
     }
 }
